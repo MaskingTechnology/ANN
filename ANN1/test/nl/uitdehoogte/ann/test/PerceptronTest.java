2744dbdf3c846caaece523393b8fc1acb51d6f1b
basmeeuwissen <bas.meeuwissen@gmail.com>
2014-09-02 20:31:59 +0200

    Perceptron Test

--- ANN1/test/nl/uitdehoogte/ann/test/PerceptronTest.java
+++ ANN1/test/nl/uitdehoogte/ann/test/PerceptronTest.java
@@ -1,5 +1,9 @@
 package nl.uitdehoogte.ann.test;
 
+import nl.uitdehoogte.ann.Perceptron;
+import nl.uitdehoogte.ann.PerceptronException;
+import nl.uitdehoogte.ann.activation.*;
+
 import org.junit.Test;
 
 import junit.framework.TestCase;
@@ -19,9 +23,82 @@ public class PerceptronTest extends TestCase
 	}
 	
 	@Test
-	public void testGetOutput()
+	public void testGetOutputLinearPositive() throws PerceptronException
 	{
-		// TODO: Implement me for real
-		assertEquals(1, 1);
+		//sum of the weights * input should be greater than zero
+		ActivationFunction activationFunction = new LinearActivationFunction();
+		Perceptron perceptron = new Perceptron(activationFunction, 2);
+		
+		double[] weights = new double[] {0.5, 1, 0.5};
+		double[] input = new double[] {1, 1};
+		double output;
+		
+		perceptron.setWeights(weights);
+		output = perceptron.getOutput(input);
+		
+		assertEquals(1.0, output, 0.0);
+	}
+	
+	public void testGetOutputLinearNegative() throws PerceptronException
+	{
+		ActivationFunction activationFunction = new LinearActivationFunction();
+		Perceptron perceptron = new Perceptron(activationFunction, 2);
+		
+		double[] weights = new double[] {0.5, 1.0, 0.5};
+		double[] input = new double[] {-1, 1.0};
+		double output;
+		
+		perceptron.setWeights(weights);
+		output = perceptron.getOutput(input);
+		
+		assertEquals(-1.0, output, 0.0);
+	}
+	
+	@Test
+	public void testGetOutputSigmoid1() throws PerceptronException
+	{
+		ActivationFunction activationFunction = new SigmoidActivationFunction();
+		Perceptron perceptron = new Perceptron(activationFunction, 2);
+		
+		double[] weights = new double[] {0.5, 1.0, 0.5};
+		double[] input = new double[] {-1, 1};
+		double output;
+		
+		perceptron.setWeights(weights);
+		output = perceptron.getOutput(input);
+		
+		assertEquals(0.5, output, 0.0);
+	}
+	
+	@Test
+	public void testGetOutputSigmoid2() throws PerceptronException
+	{
+		ActivationFunction activationFunction = new SigmoidActivationFunction();
+		Perceptron perceptron = new Perceptron(activationFunction, 2);
+		
+		double[] weights = new double[] {0.5, 1.0, 0.5};
+		double[] input = new double[] {-1, 1};
+		double output;
+		
+		perceptron.setWeights(weights);
+		output = perceptron.getOutput(input);
+		
+		assertEquals(0.5, output, 0.0);
+	}
+	
+	@Test
+	public void testGetOutputSigmoid3() throws PerceptronException
+	{
+		ActivationFunction activationFunction = new SigmoidActivationFunction();
+		Perceptron perceptron = new Perceptron(activationFunction, 2);
+		
+		double[] weights = new double[] {0.5, 0.5, 0.5};
+		double[] input = new double[] {1, 1};
+		double output;
+		
+		perceptron.setWeights(weights);
+		output = perceptron.getOutput(input);
+		
+		assertEquals(0.815, output, 0.05);
 	}
 }
