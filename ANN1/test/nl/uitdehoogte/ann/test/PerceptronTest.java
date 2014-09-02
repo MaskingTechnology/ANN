ffd65e2c372f3528cde0e54bbd51da0bfcb53600
Peter van Vliet <p.van.vliet@anyvation.com>
2014-09-02 16:17:32 +0200

    Added first test class.

---
+++ ANN1/test/nl/uitdehoogte/ann/test/PerceptronTest.java
@@ -0,0 +1,15 @@
+package nl.uitdehoogte.ann.test;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class PerceptronTest extends TestCase
+{
+	@Test
+	public void testGetOutput()
+	{
+		// TODO: Implement me for real
+		assertEquals(1, 1);
+	}
+}
